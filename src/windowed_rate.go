cac63d2decdb6eb73c0df98dc3be856d82899a2c
TheBestPessimist <cristian@tbp.land>
2018-10-26 09:53:56 +0300

    Add some explanations to WindowdRate

--- src/windowed_rate.go
+++ src/windowed_rate.go
@@ -9,6 +9,15 @@ type ratePair struct {
 	value        int64
 }
 
+/**
+The principle of this WindowedRate struct and algorithm is that it
+stores the time instant when a specific value ("uploaded quantity") was inserted,
+computes an average of all those values and then divides that average by how much time
+it took to upload them.
+
+A circular array of fixed length is used so that new records will simply replace the old ones
+as more data is uploaded.
+*/
 type WindowedRate struct {
 	arrayCapacity int
 	insertIndex   int
@@ -30,7 +39,9 @@ func NewWindowedRate(arrayCapacity int) WindowedRate {
 	return rpm
 }
 
-//noinspection GoUnusedExportedFunction,GoUnusedParameter
+/**
+InsertValue inserts a value in the circular array along with the current time
+*/
 func (rpm *WindowedRate) InsertValue(value int64) {
 	rpm.insertIndex = (rpm.insertIndex + 1) % rpm.arrayCapacity
 	if rpm.arraySize < rpm.arrayCapacity {
@@ -40,7 +51,14 @@ func (rpm *WindowedRate) InsertValue(value int64) {
 	rpm.values[rpm.insertIndex] = ratePair{time.Now(), value}
 }
 
-//noinspection GoUnusedExportedFunction,GoUnusedVariable
+/**
+ComputeAverage calculates the average transfer speed between
+the first entry in the array (firstEntry) and
+the last entry in the array (latestEntry).
+
+It handles the case where the array was not filled completely
+(we are early in the upload)
+*/
 func (rpm WindowedRate) ComputeAverage() int64 {
 	latestEntry := rpm.values[rpm.insertIndex].insertedTime
 
@@ -49,6 +67,13 @@ func (rpm WindowedRate) ComputeAverage() int64 {
 		firstEntry = rpm.values[(rpm.insertIndex+1)%rpm.arrayCapacity].insertedTime
 	}
 
+	/**
+	The sum is used to smooth-out random reansfer rate outliers
+	eg.:
+	- previous last and last transfer speeds: 100 MB, 120MB (so 20 MB transferred)
+	- current transfer: 120.1MB (so 0.1 MB transferred)
+	=> if the smoothing would not be applied, there would be a higher drop in transfer speed displayed
+	*/
 	sum := int64(0)
 	for i := 0; i < rpm.arraySize; i++ {
 		sum += rpm.values[i].value
