166f6e6266a733e49ecc28181c909cbf1eefbc1e
a-s-z-home <a-s-z-work@web.de>
2018-11-03 20:20:00 +0100

    Added string array helper functions Contains and Find.

--- src/duplicacy_utils.go
+++ src/duplicacy_utils.go
@@ -474,3 +474,24 @@ func MinInt(x, y int) int {
 	}
 	return y
 }
+
+// Contains tells whether a contains x.
+func Contains(a []string, x string) bool {
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
+	return false
+}
+
+// Find returns the smallest index i at which x == a[i],
+// or len(a) if there is no such index.
+func Find(a []string, x string) int {
+	for i, n := range a {
+		if x == n {
+			return i
+		}
+	}
+	return len(a)
+}
