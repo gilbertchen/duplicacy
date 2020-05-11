76f1274e13ae316cc8c4d2e07ced12df131af5a2
Gilbert Chen <gchen@acrosync.com>
2020-05-10 21:41:30 -0400

    Bump version to 2.5.2

--- duplicacy/duplicacy_main.go
+++ duplicacy/duplicacy_main.go
@@ -2084,7 +2084,7 @@ func main() {
 	app.Name = "duplicacy"
 	app.HelpName = "duplicacy"
 	app.Usage = "A new generation cloud backup tool based on lock-free deduplication"
-	app.Version = "2.5.1" + " (" + GitCommit + ")"
+	app.Version = "2.5.2" + " (" + GitCommit + ")"
 
 	// If the program is interrupted, call the RunAtError function.
 	c := make(chan os.Signal, 1)
