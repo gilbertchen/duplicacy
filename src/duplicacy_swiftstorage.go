0590daff8595510f834714c0c2390d339e71479d
Mark Feit <mfeit@notonthe.net>
2021-12-04 10:53:34 -0500

    More dev

--- src/duplicacy_swiftstorage.go
+++ src/duplicacy_swiftstorage.go
@@ -108,7 +108,8 @@ func CreateSwiftStorage(storageURL string, key string, threads int) (storage *Sw
 		arguments["protocol"] = "https"
 	}
 
-	ctx := context.TODO
+	ctx, cancel := context.WithDeadline(context.Background(), time.Millisecond)
+	defer cancel()
 
 	// Please refer to https://godoc.org/github.com/ncw/swift#Connection
 	connection := swift.Connection{
