54952cef26563d376e4a273e05973a0038e811bc
Gilbert Chen <gchen@acrosync.com>
2022-07-10 12:23:11 -0400

    Fixed a bug that referenced uninitialized operator.snapshotCache

--- src/duplicacy_chunkoperator.go
+++ src/duplicacy_chunkoperator.go
@@ -489,7 +489,7 @@ func (operator *ChunkOperator) UploadChunk(threadIndex int, task ChunkTask) bool
 		chunk.VerifyID()
 	}
 
-	if task.isMetadata && operator.storage.IsCacheNeeded() {
+	if task.isMetadata && operator.snapshotCache != nil && operator.storage.IsCacheNeeded() {
 		// Save a copy to the local snapshot.
 		chunkPath, exist, _, err := operator.snapshotCache.FindChunk(threadIndex, chunkID, false)
 		if err != nil {
