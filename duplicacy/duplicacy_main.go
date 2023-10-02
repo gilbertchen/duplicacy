fd3bceae19c544f689afe36afcc45bda97903fd7
Gilbert Chen <gchen@acrosync.com>
2023-10-02 12:30:23 -0400

    Bump version to 3.2.1

--- duplicacy/duplicacy_main.go
+++ duplicacy/duplicacy_main.go
@@ -2262,7 +2262,7 @@ func main() {
 	app.Name = "duplicacy"
 	app.HelpName = "duplicacy"
 	app.Usage = "A new generation cloud backup tool based on lock-free deduplication"
-	app.Version = "3.2.0" + " (" + GitCommit + ")"
+	app.Version = "3.2.1" + " (" + GitCommit + ")"
 
 	// Exit with code 2 if an invalid command is provided
 	app.CommandNotFound = func(context *cli.Context, command string) {
